c8e94ab448339e1e258693db567ac014eb4199af
IFSP <IFSP@LAB1PROF.labs.ifspcapivari.edu.br>
2016-03-22 15:39:04 -0300

    Alteracao da home_view. Add ModalSenha e desabilitou todos os campos do form

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,28 +1,54 @@
 <h1>Dados Pessoais</h1>
-<form>
     <div class="form-group">
         <label for="nome">Nome:</label>
-        <input type="text" class="form-control" id="nome" placeholder="Nome" value="<?php echo $docente->nome ?>" readonly>
+        <input type="text" class="form-control" id="nome" value="<?php echo $docente->nome ?>" readonly>
+    </div>
+    <div class="form-group">
+        <label for="link">Link para sua página:</label>
+        <input type="text" class="form-control" id="link" value="<?php echo "http://www.ifspcapivari.com.br/corpo-docente/" . $docente->slug ?>" readonly>
     </div>
     <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Email" value="<?php echo $docente->email ?>">
+        <input type="email" class="form-control" id="email" value="<?php echo $docente->email ?>" readonly>
     </div>
     <div class="form-group">
         <label for="perfil">Perfil:</label>
-        <select class="form-control">
-            <option value="Docente">Docente</option>
-            <option value="Administrador">Administrador</option>
-            <option value="Sóciopedagógico">Sóciopedagógico</option>
-        </select>
+        <input type="text" class="form-control" id="perfil" value="<?php echo $docente->perfil ?>" readonly>
     </div>
     <div class="form-group">
         <label for="usuario">Usuário:</label>
         <input type="text" class="form-control" id="usuario" value="<?php echo $docente->usuario ?>" readonly>
     </div>
     <div class="form-group">
-        <label for="senha">Senha:</label>
-        <input type="password" class="form-control" id="senha">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalSenha" title="Clique para alterar sua senha de acesso ao SigProf">
+            Alterar Senha
+        </button>
     </div>
-  <button type="submit" class="btn btn-success">Salvar Dados</button>
-</form>
+
+<!-- Modal Senha -->
+<div class="modal fade" id="modalSenha" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Alterar Senha</h4>
+      </div>
+        <form method="post" action="">
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="senha">Nova Senha:</label>
+                    <input type="password" class="form-control" id="senha" name="senha" placeholder="Nova Senha">
+                </div>
+                <div class="form-group">
+                    <label for="confirmarSenha">Confirmar Senha:</label>
+                    <input type="password" class="form-control" id="confirmarSenha" name="confirmarsenha" placeholder="Confirmar Senha Senha">
+                </div>
+            </div>
+            <div class="modal-footer">
+                 <button type="button" class="btn btn-success">Alterar Senha</button>
+                 <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
