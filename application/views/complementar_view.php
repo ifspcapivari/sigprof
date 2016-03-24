aee1502471b12a7fd99f2b12d65802cc3b868139
Rafael Pinheiro <rafaelwendel@hotmail.com>
2016-03-24 10:23:35 -0300

    Menu estilo tabs na pagina de info complementares

--- application/views/complementar_view.php
+++ application/views/complementar_view.php
@@ -1,27 +1,40 @@
 <h1>Informações Complementares</h1>
-<form method="post" action="">
-    <div class="form-group">
-        <img src="<?php echo base_url('assets/img/profile-default.png') ?>" alt="..." class="img-thumbnail">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalFotoPerfil">
-            Alterar imagem
+<!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+    <li role="presentation"><a href="#disciplinas" aria-controls="disciplinas" role="tab" data-toggle="tab">Disciplinas</a></li>
+  </ul>
+<br />
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane fade in active" id="home">
+        <form method="post" action="">
+            <div class="form-group">
+                <img src="<?php echo base_url('assets/img/profile-default.png') ?>" alt="..." class="img-thumbnail">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalFotoPerfil">
+                    Alterar imagem
+                </button>
+            </div>
+            <div class="form-group">
+                <label for="curriculo_lattes">Currículo Lattes:</label>
+                <input type="text" class="form-control" id="curriculo_lattes" name="curriculo_lattes" value="<?php echo "" ?>" placeholder="Insira aqui o link do seu currículo lattes" >
+            </div>
+            <div class="form-group">
+                <label for="titulacao">Titulação:</label>
+                <input type="text" class="form-control" id="titulacao" name="titulacao" value="<?php echo "" ?>" placeholder="Ex: Doutor em Química" >
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição:</label>
+                <textarea class="form-control" id="descricao" name="descricao" rows="7" placeholder="Coloque aqui uma breve descrição sobre você"></textarea>
+            </div>
+        <button type="submit" class="btn btn-success" id="btnAtualizar" name="btnAtualizar">
+            Atualizar Dados
         </button>
+        </form>
     </div>
-    <div class="form-group">
-        <label for="curriculo_lattes">Currículo Lattes:</label>
-        <input type="text" class="form-control" id="curriculo_lattes" name="curriculo_lattes" value="<?php echo "" ?>" placeholder="Insira aqui o link do seu currículo lattes" >
+    <div role="tabpanel" class="tab-pane fade" id="disciplinas">
+        <p>Disciplinas</p>
     </div>
-    <div class="form-group">
-        <label for="titulacao">Titulação:</label>
-        <input type="text" class="form-control" id="titulacao" name="titulacao" value="<?php echo "" ?>" placeholder="Ex: Doutor em Química" >
-    </div>
-    <div class="form-group">
-        <label for="descricao">Descrição:</label>
-        <textarea class="form-control" id="descricao" name="descricao" rows="7" placeholder="Coloque aqui uma breve descrição sobre você"></textarea>
-    </div>
-<button type="submit" class="btn btn-success" id="btnAtualizar" name="btnAtualizar">
-    Atualizar Dados
-</button>
-</form>
+</div>
 <!-- Modal Senha -->
 <div class="modal fade" id="modalFotoPerfil" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
